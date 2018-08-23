d274147a4df9edeecf696c599582aa228fe1f5a7
Fedir kantur <fedichk@gmail.com>
2018-08-23 07:55:40 +0300

    Added implementation to run tests programmatically

---
+++ src/test/java/TestsRunClass.java
@@ -0,0 +1,42 @@
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TagFilter;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+
+import java.util.List;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
+public class TestsRunClass {
+
+    public static void main(String[] args) {
+        String[] filters = {"syntax", "functionality"};
+        if (args.length > 0) {
+            filters = args;
+        }
+
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(
+                        selectClass("GetMethodTest"),
+                        selectClass("PostMethodTest"))
+                .filters(
+                        TagFilter.includeTags(filters)
+                )
+                .build();
+
+        Launcher launcher = LauncherFactory.create();
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+
+        TestExecutionSummary summary = listener.getSummary();
+        long testFoundCount = summary.getTestsFoundCount();
+        List<TestExecutionSummary.Failure> failures = summary.getFailures();
+        System.out.println("getTestsSucceededCount() - " + summary.getTestsSucceededCount());
+        failures.forEach(failure -> System.out.println("failure - " + failure.getException()));
+        System.out.println("Founding tests: " + testFoundCount);
+    }
+}
